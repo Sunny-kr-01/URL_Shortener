cf7deafcc7981c4fcd62a7e09f4461d421044bb0
Sunny-kr-01 <2405984@kiit.ac.in>
2025-12-27 02:27:32 +0530

    Done with connection of mongoDB

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express =require ('express')
-
+const urlRouter=require('./routes/url')
 const app= express()
 const PORT= 67
 
+app.use('/url',urlRouter)
 
 app.listen(PORT,()=>{
     console.log(`App is listening on PORT ${PORT}`)

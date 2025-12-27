cc7f44a8d14821c709294a8ad4a9ff39772b5223
Sunny-kr-01 <2405984@kiit.ac.in>
2025-12-28 05:11:40 +0530

    Done with table

--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -1,9 +1,12 @@
 const express =require ('express')
-
+const URL=require('../models/url')
 const router = express.Router();
 
-router.get('/',(req,res)=>{
-    return res.render('home')
+router.get('/',async(req,res)=>{
+    const all_urls=await URL.find({});
+    return res.render('home',
+        {urls:all_urls}
+    )
 })
 
 

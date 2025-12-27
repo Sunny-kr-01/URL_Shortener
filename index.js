495e03682c7074983ff074607d2c37eb4197cd91
Sunny-kr-01 <2405984@kiit.ac.in>
2025-12-28 00:55:58 +0530

    static routers for frontend

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const express =require ('express')
+const path=require('path')
 const urlRouter=require('./routes/url')
+const statisRoute=require('./routes/staticRouter')
 const {connectMongoose}=require('./connect')
 const app= express()
 const PORT= 67
 
 app.use(express.urlencoded({extended:false}))
 
+//what view engine we are going to use
+app.set ('view engine','ejs')
+
+//aslo give the ejs file location
+app.set('views',path.resolve('./views'))
+
 connectMongoose('mongodb://127.0.0.1:27017/Short_URLs')
 .then(()=>{console.log("MongoDB connected")})
 .catch((err)=>{console.log("Mongo Error")})
 
 app.use('/url',urlRouter)
-
+app.use('/',statisRoute)
 app.listen(PORT,()=>{
     console.log(`App is listening on PORT ${PORT}`)
 })
--- routes/url.js
+++ routes/url.js
@@ -3,13 +3,7 @@ const {GenerateNEwShortURL,RedirectSite,showStats}=require('../controllers/url')
 
 const router =express.Router();
 
-router.route('/')
-.get((req,res)=>{
-    res.send(`
-        <h1>URL Shortner</h1>
-        `)
-})
-.post(GenerateNEwShortURL)
+router.post('/',GenerateNEwShortURL)
 
 router.route('/:id')
 .get(RedirectSite)

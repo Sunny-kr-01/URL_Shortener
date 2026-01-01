f305121f40287663d5b4de52fe7a3e7241d1f03a
Sunny-kr-01 <2405984@kiit.ac.in>
2026-01-02 02:40:56 +0530

    Used headers.authoristion

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,10 +2,10 @@ const {getUser}=require('../service/auth')
 
 async function restrictToLoggedInUserOnly(req,res,next){
     // const userID=req.cookies?.uid;
-    const userID=req.headers['Authorization']; // so now we got the token whcih we stored as uid 
+    const userID=req.headers.authorization; // so now we got the token whcih we stored as uid 
 
     if(!userID) return res.redirect('/login')
-    const token=userID.split('Bearer')[1];//split convert it into array [0]th is pace (" ") probably
+    const token=userID.split(' ')[1];//split convert it into array [0]th is pace (" ") probably
     const user = getUser(token);
 
     if(!user) return res.redirect('/login')
@@ -14,8 +14,12 @@ async function restrictToLoggedInUserOnly(req,res,next){
 }
 async function checkAuth(req,res,next){
     // const userID=req.cookies?.uid;
-    const userID=req.headers['authorization'];
-    const token = userID.split('Bearer')[1];
+    const userID=req.headers.authorization;
+    if (!userID) {
+    req.user = null;
+    return next();
+    }
+    const token = userID.split(' ')[1];
     const user = getUser(token);
 
     req.user=user
